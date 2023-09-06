a61e8ee10509c312ea19ff0f4cf2b617f899ef25
akmz <balqeesalqudah97@gmail.com>
2023-09-06 16:29:37 +0300

    lab11 done

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# songr
+# Songr Application
+
+This is a simple web application called "Songer" that allows users to view album information and create custom
+hello messages.
+
+## Prerequisites
+
+- Java Development Kit (JDK) 17 or higher
+- Maven (for building the project)
+
+## Dependencies
+
+- Spring Boot DevTools
+- Thymeleaf (for rendering HTML templates)
+- Maven (for project management)
+- Spring MVC
+
+## Run application
+
+1. **Clone the Repository**
+
+   ```bash
+   git clone https://github.com/balqeesqud/songr.git
+
+2. **Build the Project**
+   ```bash
+   cd Songer
+
+3. **Build the project using Maven**
+   ```bash
+   mvn clean install
+
+4. Start the Spring Boot Application
+    ```bash
+   mvn spring-boot:run
+
+
+application will start on port 8080 by default. It can be accessed using web browser at http://localhost:8080.
+   
+
+
--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.songr;
 
+import com.example.songr.routes.Album;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SongrApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
+	// Constructer Testing
+	@Test
+	public void testConstructor() {
+	Album album = new Album("My Way", "Frank Sinatra", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxaCK0-8U9_D753xkMR31ncIvKhhpe7ZRysODmLyuZw&s", 8, 120);
+
+
+	assertEquals("My Way", album.getTitle());
+	assertEquals("Frank Sinatra", album.getArtist());
+	assertEquals(8, album.getSongCount());
+	assertEquals("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxaCK0-8U9_D753xkMR31ncIvKhhpe7ZRysODmLyuZw&s", album.getImageUrl());
+}
+
+
+    // Getters & Setter testing
+	@Test
+	public void testGettersAndSetters() {
+		Album album = new Album();
+
+		album.setTitle("Hello");
+		album.setArtist("Adele");
+		album.setSongCount(12);
+		album.setImageUrl("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs3pFq-3-QyNuZ_BXfPDBR67oJlkNiDXmChRLjYrS8FQ&s");
+
+		// Verify that getters return the updated values
+		assertEquals("Hello", album.getTitle());
+		assertEquals("Adele", album.getArtist());
+		assertEquals(12, album.getSongCount());
+		assertEquals("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs3pFq-3-QyNuZ_BXfPDBR67oJlkNiDXmChRLjYrS8FQ&s", album.getImageUrl());
+	}
 }
